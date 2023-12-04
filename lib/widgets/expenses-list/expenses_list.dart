cc4fb517987e90731f092fda66024bc2ab73a07c
its-asimiqbal <its.asim.iqball@gmail.com>
2023-12-04 11:24:21 +0500

    files updated

--- lib/widgets/expenses-list/expenses_list.dart
+++ lib/widgets/expenses-list/expenses_list.dart
@@ -16,11 +16,7 @@ class ExpensesList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => Dismissible(
-        key: ValueKey( expenses[index]),
-        onDismissed:(direction) {
-          onRemoveExpense(expenses[index]);
-        },
+      itemBuilder: (ctx, index) => Dismissible(key: ValueKey(expenses[index]),
         child: ExpenseItem(
           expenses[index],
         ),
