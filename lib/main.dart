26bc47b83fa0b0b6de92000d9241c4afead7f252
its-asimiqbal <its.asim.iqball@gmail.com>
2023-12-06 10:05:07 +0500

    files updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,41 @@
 import 'package:flutter/material.dart';
 import 'screens/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.normal,
+                color: kColorScheme.secondaryContainer,
+              ),
+            ),
+      ),
       home: const Expenses(),
     ),
   );
